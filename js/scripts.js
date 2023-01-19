198c6bf288ed4c54f1dc94b281609a3f73eef2ae
Plan Sarmiento <psarmiento20_565@alu.bue.edu.ar>
2023-01-19 02:47:33 -0300

    Uso de fetch

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,14 @@ class Producto {
     }
 }
 
-const libroUno = new Producto(1, "Los Compas y el diamantito legendario", 4500, "img/diamantitolegendario.jpg");
-const libroDos = new Producto(2, "Los Compas escapan de la prisión", 4700, "img/escapandeprision.jpg");
-const libroTres = new Producto(3, "Los Compas y la cámara del tiempo", 5000, "img/camaradeltiempo.jpg");
-const libroCuatro = new Producto(4, "Los Compas y la Maldición de Mikecrack", 7000, "img/maldicionmikecrack.jpg");
-const libroCinco = new Producto(5, "Los Compas y la Entidad.Exe", 5500, "img/entidadexe.webp");
-const libroSeis = new Producto(6, "Los Compas perdidos en el espacio", 6500, "img/perdidoespacio.jpg");
+const libroUno = new Producto(1, "Los Compas y el diamantito legendario", 45, "img/diamantitolegendario.jpg");
+const libroDos = new Producto(2, "Los Compas escapan de la prisión", 47, "img/escapandeprision.jpg");
+const libroTres = new Producto(3, "Los Compas y la cámara del tiempo", 50, "img/camaradeltiempo.jpg");
+const libroCuatro = new Producto(4, "Los Compas y la Maldición de Mikecrack", 70, "img/maldicionmikecrack.jpg");
+const libroCinco = new Producto(5, "Los Compas y la Entidad.Exe", 55, "img/entidadexe.webp");
+const libroSeis = new Producto(6, "Los Compas perdidos en el espacio", 65, "img/perdidoespacio.jpg");
 
-/* Array de productos */
+/*Array de productos*/
 const productos = [libroUno, libroDos, libroTres, libroCuatro, libroCinco, libroSeis];
 
 let carrito = [];
@@ -27,7 +27,6 @@ if (localStorage.getItem("carrito")) {
 
 const contenedorProductos = document.getElementById("contenedorProductos");
 
-
 const mostrarProductos = () => {
     productos.forEach(producto => {
         const card = document.createElement("div");
@@ -37,7 +36,7 @@ const mostrarProductos = () => {
                     <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
                     <div class= "card-body">
                         <h5>${producto.nombre}</h5>
-                        <p> ${producto.precio} </p>
+                        <p>USD ${producto.precio} </p>
                         <button class="btn colorBoton" id="boton${producto.id}" > Añadir </button>
                     </div>
                 </div>
@@ -47,7 +46,7 @@ const mostrarProductos = () => {
         const boton = document.getElementById(`boton${producto.id}`);
         boton.addEventListener("click", () => {
             agregarAlCarrito(producto.id);
-            Toastify( {
+            Toastify({
                 text: "Producto Agregado al Carrito",
                 duration: 2000,
                 position: "right",
@@ -144,9 +143,27 @@ const calcularTotal = () => {
     carrito.forEach(producto => {
         totalCompra = totalCompra + producto.precio * producto.cantidad;
     })
-    total.innerHTML = ` $${totalCompra}`;
+    total.innerHTML = ` USD ${totalCompra}`;
 }
 
-setTimeout (() => {
+
+// Uso de Fetch
+
+const valorDolar = "https://criptoya.com/api/dolar";
+
+const dolar = document.getElementById("cotizacionDolar");
+
+setInterval(() => {
+    fetch(valorDolar)
+        .then(response => response.json())
+        .then(({ blue }) => {
+            cotizacionDolar.innerHTML = ` 
+            <h3> $ ${blue} </h3>
+            `
+        })
+        .catch(error => console.log(error))
+}, 5000)
+
+setTimeout(() => {
     document.body.style.backgroundColor = "red";
 }, 3000)
